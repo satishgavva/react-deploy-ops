e6834d145ffcca70738e7272e6adbe0ddfb19c94
satishgavva <127138156+satishgavva@users.noreply.github.com>
2023-03-23 11:51:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!!!!!--Github action.!! lat1
+          Edit <code>src/App.js</code> and save to reload!!!!!--Github action.!! satish runner
         </p>
         <a
           className="App-link"
